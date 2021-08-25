6717b188a06a805b8b6a8740893d1de1ee9000b7
Rafael Colombo <rcolombodev@gmail.com>
2021-08-25 20:04:33 -0300

    Adiciona decimo oitavo Requisito

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputEmail = document.getElementById('email-login');
 const inputPassword = document.getElementById('input-password');
 const login = 'tryber@teste.com';
 const password = '123456';
+const agreement = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
 
 function verifyLogin() {
   if (inputEmail.value === login && inputPassword.value === password) {
@@ -13,3 +15,13 @@ function verifyLogin() {
 }
 
 buttonLogin.addEventListener('click', verifyLogin);
+
+function verifySumbit() {
+  if (agreement.checked) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+}
+
+agreement.addEventListener('click', verifySumbit);
